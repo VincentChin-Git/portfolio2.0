1b78274667d972c1c6481bff0b5bbaa24519e70e
VincentChin-Git <124128510+VincentChin-Git@users.noreply.github.com>
2023-07-01 23:39:24 +0800

    chore: view project detail

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -1,18 +1,60 @@
+import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/router";
+import { NextSeo } from "next-seo";
 import { useEffect } from "react";
 
-// import dataProject1 from "./project_1";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import dataProject1 from "@/utils/project_1";
+import dataProject2 from "@/utils/project_2";
 
 export default function Project() {
   const searchParams = useSearchParams();
   const project = searchParams.get("project");
   const router = useRouter();
-  //   const config = dataProject1;
+  const config =
+    project == "1"
+      ? dataProject1
+      : project == "2"
+      ? dataProject2
+      : { title: "", desc: "", imgList: [] };
 
   useEffect(() => {
     if (!project) router.push("/");
   });
 
-  return <div></div>;
+  return (
+    <div className=" flex flex-col min-h-screen ">
+      <NextSeo
+        title="Vincent Chin | Portfolio"
+        description="This is a portfolio website made by Vincent"
+      />
+      <Header />
+
+      <div className=" container mx-auto my-12 text-center">
+        <div className=" font-bold my-8 text-2xl sm:text-3xl">
+          {config.title}
+        </div>
+        <div className=" my-8 sm:text-xl mx-4">{config.desc}</div>
+        <div className=" flex flex-wrap justify-center items-center gap-y-8 ">
+          {config.imgList.map((img, iIndex) => {
+            return (
+              <div className=" w-full lg:w-1/2 box-border px-4 " key={iIndex}>
+                <Image
+                  className="hover:scale-110 cursor-pointer"
+                  src={img.src}
+                  alt=""
+                />
+                <div className=" my-4">{img.name}</div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className="flex-1"></div>
+      <Footer />
+    </div>
+  );
 }
---
+++ src/utils/project_2.ts
@@ -0,0 +1,47 @@
+import img_01 from "@/assets/projects/project_2/p2_01.png";
+import img_02 from "@/assets/projects/project_2/p2_02.png";
+import img_03 from "@/assets/projects/project_2/p2_03.png";
+import img_04 from "@/assets/projects/project_2/p2_04.png";
+import img_05 from "@/assets/projects/project_2/p2_05.png";
+import img_06 from "@/assets/projects/project_2/p2_06.png";
+import img_07 from "@/assets/projects/project_2/p2_07.png";
+import img_08 from "@/assets/projects/project_2/p2_08.png";
+import img_09 from "@/assets/projects/project_2/p2_09.png";
+import img_10 from "@/assets/projects/project_2/p2_10.png";
+import img_11 from "@/assets/projects/project_2/p2_11.png";
+import img_12 from "@/assets/projects/project_2/p2_12.png";
+import img_13 from "@/assets/projects/project_2/p2_13.png";
+import img_14 from "@/assets/projects/project_2/p2_14.png";
+import img_15 from "@/assets/projects/project_2/p2_15.png";
+import img_16 from "@/assets/projects/project_2/p2_16.png";
+import img_17 from "@/assets/projects/project_2/p2_17.png";
+import img_18 from "@/assets/projects/project_2/p2_18.png";
+import img_19 from "@/assets/projects/project_2/p2_19.png";
+const dataProject2 = {
+  title: "E-Commerce Website",
+  desc: "Built for the clients of the printing industry.",
+  imgList: [
+    { src: img_01, name: "Home Page" },
+    { src: img_02, name: "Home Page Part 2" },
+    { src: img_03, name: "Register Page" },
+    { src: img_04, name: "Register Page Part 2" },
+    { src: img_05, name: "Login Page" },
+    { src: img_06, name: "Product Category" },
+    { src: img_07, name: "Product Category Part 2 ( With Video )" },
+    { src: img_08, name: "Product Detail With Custom Size" },
+    { src: img_09, name: "Product Detail With Option" },
+    { src: img_10, name: "Product Detail With Preset Sizes" },
+
+    { src: img_11, name: "Product Detail With Min. / Max. Custom Sizes" },
+    { src: img_12, name: "Product Detail With Option & Width" },
+    { src: img_13, name: "Product Detail With Max. Quantity" },
+    { src: img_14, name: "Video Provided to Show Additional Info" },
+    { src: img_15, name: "Upload Artwork Page" },
+    { src: img_16, name: "Upload Artwork & View Before Submit" },
+    { src: img_17, name: "Check Cart & View Order Summary" },
+    { src: img_18, name: "Choose Delivery Method" },
+    { src: img_19, name: "View Uploaded Artwork" },
+  ],
+};
+
+export default dataProject2;
