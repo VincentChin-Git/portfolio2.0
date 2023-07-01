9ca4cfd99c9fffb063d70b6147996fba2390b74a
VincentChin-Git <124128510+VincentChin-Git@users.noreply.github.com>
2023-07-01 17:13:59 +0800

    chore: setup sendgrid email

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { Button, Input, Textarea, useToast } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { useState } from "react";
 
@@ -6,12 +7,40 @@ import XIcon from "@/assets/svg/XIcon";
 
 const Header = () => {
   const [show, setShow] = useState(false);
+  const [showContact, setShowContact] = useState(false);
+  const [emailInfo, setEmailInfo] = useState({ name: "", email: "", msg: "" });
   const router = useRouter();
+  const toast = useToast();
+  const handleSentEmail = async () => {
+    try {
+      const res = await fetch("/api/sendemail", {
+        body: JSON.stringify({ ...emailInfo }),
+        headers: { "Content-Type": "application/json" },
+        method: "POST",
+      });
+
+      await res.json();
+      if (res.ok) {
+        setEmailInfo({ name: "", email: "", msg: "" });
+        setShowContact(false);
+        return toast({
+          title: "Success",
+          description:
+            "Thank you for your interest. I will contact you shortly.",
+          status: "success",
+          duration: 5000,
+          isClosable: true,
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
   const navs = [
-    { path: "/", title: "Home" },
-    { path: "/skillset", title: "Skills Set" },
-    { path: "/projects", title: "Projects" },
-    { path: "/contact", title: "Contact Me" },
+    { do: () => router.push("/"), title: "Home" },
+    { do: () => router.push("/skillset"), title: "Skills Set" },
+    { do: () => router.push("/projects"), title: "Projects" },
+    { do: () => setShowContact(!showContact), title: "Contact Me" },
   ];
   return (
     // switch to mobile mode in md
@@ -30,7 +59,7 @@ const Header = () => {
               <div
                 className=" hover:text-primary cursor-pointer font-medium"
                 key={item.title}
-                onClick={() => router.push(item.path)}
+                onClick={item.do}
               >
                 {item.title}
               </div>
@@ -61,18 +90,78 @@ const Header = () => {
       >
         {navs.map((nav) => (
           <div
-            key={nav.path}
+            key={nav.title}
             className=" font-medium my-8 cursor-pointer hover:font-bold"
           >
             {nav.title}
           </div>
         ))}
       </div>
+
+      {showContact && (
+        <div
+          className="fixed left-0 top-0 right-0 bottom-0 bg-black z-50 flex items-center justify-center"
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}
+          onClick={() => setShowContact(false)}
+        >
+          <div
+            className=" bg-white rounded-xl max-w-[500px] w-[90%] p-4 sm:p-6"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <div className=" flex justify-between items-center">
+              <div className=" font-bold text-lg sm:text-xl ml-2">
+                Contact Me
+              </div>
+              <div
+                className=" cursor-pointer"
+                onClick={() => setShowContact(false)}
+              >
+                <XIcon size="40px" color="#333" />
+              </div>
+            </div>
+
+            {/* input box */}
+            <div className=" my-4 mx-2 flex flex-col gap-y-4">
+              <Input
+                value={emailInfo.name}
+                onChange={(e) =>
+                  setEmailInfo({ ...emailInfo, name: e.target.value })
+                }
+                placeholder="Name"
+              />
+
+              <Input
+                value={emailInfo.email}
+                onChange={(e) =>
+                  setEmailInfo({ ...emailInfo, email: e.target.value })
+                }
+                placeholder="Email"
+              />
+
+              <Textarea
+                placeholder="Leave your msg here..."
+                rows={8}
+                value={emailInfo.msg}
+                onChange={(e) =>
+                  setEmailInfo({ ...emailInfo, msg: e.target.value })
+                }
+              />
+            </div>
+            <div className=" mx-2 my-4">
+              <Button
+                bg={"#D6E8DB"}
+                _hover={{ backgroundColor: "#C1D0B5" }}
+                px={6}
+                onClick={handleSentEmail}
+              >
+                Submit
+              </Button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
 
 export default Header;
-{
-  /*  */
-}
---
+++ src/pages/api/sendemail.ts
@@ -0,0 +1,36 @@
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+import mailSender from "@sendgrid/mail";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY || "";
+const SENDGRID_EMAIL_FROM = process.env.SENDGRID_EMAIL_FROM || "";
+const SENDGRID_EMAIL_TO = process.env.SENDGRID_EMAIL_TO || "";
+mailSender.setApiKey(SENDGRID_API_KEY);
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  const { name, email, msg } = req.body;
+
+  const emailTemplate = {
+    to: SENDGRID_EMAIL_TO, // Change to your recipient
+    from: SENDGRID_EMAIL_FROM, // Change to your verified sender
+    subject: "Message Sent from Portfolio",
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${msg}`,
+  };
+
+  console.log(
+    SENDGRID_API_KEY,
+    SENDGRID_EMAIL_FROM,
+    SENDGRID_EMAIL_TO,
+    emailTemplate
+  );
+
+  try {
+    await mailSender.send(emailTemplate);
+    console.log("Email Sent!");
+    return res.status(200).json({});
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Error" });
+  }
+}
