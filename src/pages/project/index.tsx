a214cdce3798ff73aabd194412b5324d0a69c871
VincentChin-Git <124128510+VincentChin-Git@users.noreply.github.com>
2023-07-01 21:02:19 +0800

    fix: error

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -2,13 +2,13 @@ import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
-import dataProject1 from "./project_1";
+// import dataProject1 from "./project_1";
 
 export default function Project() {
   const searchParams = useSearchParams();
   const project = searchParams.get("project");
   const router = useRouter();
-  const config = dataProject1;
+  //   const config = dataProject1;
 
   useEffect(() => {
     if (!project) router.push("/");
