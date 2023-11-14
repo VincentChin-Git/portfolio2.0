fc70b0a13e9cdbee6d5d70e45b4b7c56c0a5d03d
VincentChin-Git <124128510+VincentChin-Git@users.noreply.github.com>
2023-11-14 11:09:15 +0800

    fix: lint error

--- src/utils/project_2.ts
+++ src/utils/project_2.ts
@@ -1,22 +1,3 @@
-import img_01 from "@/assets/projects/project_2/p2_01.png";
-import img_02 from "@/assets/projects/project_2/p2_02.png";
-import img_03 from "@/assets/projects/project_2/p2_03.png";
-import img_04 from "@/assets/projects/project_2/p2_04.png";
-import img_05 from "@/assets/projects/project_2/p2_05.png";
-import img_06 from "@/assets/projects/project_2/p2_06.png";
-import img_07 from "@/assets/projects/project_2/p2_07.png";
-import img_08 from "@/assets/projects/project_2/p2_08.png";
-import img_09 from "@/assets/projects/project_2/p2_09.png";
-import img_10 from "@/assets/projects/project_2/p2_10.png";
-import img_11 from "@/assets/projects/project_2/p2_11.png";
-import img_12 from "@/assets/projects/project_2/p2_12.png";
-import img_13 from "@/assets/projects/project_2/p2_13.png";
-import img_14 from "@/assets/projects/project_2/p2_14.png";
-import img_15 from "@/assets/projects/project_2/p2_15.png";
-import img_16 from "@/assets/projects/project_2/p2_16.png";
-import img_17 from "@/assets/projects/project_2/p2_17.png";
-import img_18 from "@/assets/projects/project_2/p2_18.png";
-import img_19 from "@/assets/projects/project_2/p2_19.png";
 const dataProject2 = {
   title: "E-Commerce Website",
   desc: "Built for the clients of the printing industry.",
